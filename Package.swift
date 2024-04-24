675e9d4dd5584973b22a2329e0cc13fc5e96993a
Isaac Paul <izackp@gmail.com>
2024-04-24 12:43:48 -0400

    wrong urls

--- Package.swift
+++ Package.swift
@@ -12,12 +12,12 @@ let package = Package(
     targets: [
         .binaryTarget(
                     name: "Sentry",
-                    url: "https://github.com/getsentry/sentry-cocoa/releases/download/8.24.1-alpha.0/Sentry.xcframework.zip",
+                    url: "https://github.com/izackp/sentry-cocoa/releases/download/8.24.1-alpha.0/Sentry.xcframework.zip",
                     checksum: "7723c72733c887c13e6b25039a4d5822f858f538b1357734d32835336be4235b" //Sentry-Static
                 ),
         .binaryTarget(
                     name: "Sentry-Dynamic",
-                    url: "https://github.com/getsentry/sentry-cocoa/releases/download/8.24.1-alpha.0/Sentry-Dynamic.xcframework.zip",
+                    url: "https://github.com/izackp/sentry-cocoa/releases/download/8.24.1-alpha.0/Sentry-Dynamic.xcframework.zip",
                     checksum: "616fb0fb0661b4e8aeec2b89c2594a0e1c5d476a65a445db1fc623c56b568756" //Sentry-Dynamic
                 ),
         .target ( name: "SentrySwiftUI",
